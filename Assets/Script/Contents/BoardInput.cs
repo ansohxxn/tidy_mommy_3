14432188309869dc66bdd38e24638fb022f93622
DESKTOP-S1KVEJK\소현제현 <lobreamer@daum.net>
2021-07-05 10:27:50 +0900

    함수 세분화

--- Assets/Script/Contents/BoardInput.cs
+++ Assets/Script/Contents/BoardInput.cs
@@ -16,16 +16,9 @@ public class BoardInput : MonoBehaviour, IPointerClickHandler, IPointerDownHandl
 
         if (Managers.Game.prevClickedCol == Define.Column.None) // 클릭 상태였던 블록이 없다면 selectedFrame 활성화
         {
-            int col = (int)colID;
-            if (board.IsBlockEmpty(col)) 
-                return;
-            Managers.Game.prevClickedCol = colID;
-
-            int row = board.Get_Last_Row_Block_Index((int)colID);
-            if (row >= Define.MAX_ROW_NUM) 
-                return;
-
-            Managers.Game.selectedFrame.Setting(board.GetLastRowBlock((int)colID), pos.blockPos[col,row]);
+            Transform pos = Get_Selected_Frame_Pos();
+            if (pos != null) 
+                Managers.Game.selectedFrame.Setting(board.GetLastRowBlock((int)colID), pos);
         }
         else if (Managers.Game.prevClickedCol == colID)  // 클릭 했던 열 동일하게 또 누르면 selectedFrame 해제 
         {
@@ -37,6 +30,20 @@ public class BoardInput : MonoBehaviour, IPointerClickHandler, IPointerDownHandl
             board.Move_And_Remove(Managers.Game.selectedFrame.selectedBlock, (int)Managers.Game.prevClickedCol, (int)colID);
             Set_Basic_State_Sprite();
         }
+    }
+
+    private Transform Get_Selected_Frame_Pos()
+    {
+        int col = (int)colID;
+        if (board.IsBlockEmpty(col))
+            return null;
+        Managers.Game.prevClickedCol = colID;
+
+        int row = board.Get_Last_Row_Block_Index((int)colID);
+        if (row >= Define.MAX_ROW_NUM)
+            return null;
+
+        return pos.blockPos[col, row];
     }
 
     private void Set_Basic_State_Sprite()
--- Assets/Script/Contents/Pos.cs
+++ Assets/Script/Contents/Pos.cs
@@ -7,8 +7,8 @@ public class Pos : MonoBehaviour
     float width, height;
     Vector2 startPos;
 
-    public Transform[,] blockPos = new Transform[3, 10];
-    public ParticleSystem[,] particlePos = new ParticleSystem[3, 10];
+    public Transform[,] blockPos = new Transform[Define.MAX_COL_NUM, Define.MAX_ROW_NUM];
+    public ParticleSystem[,] particlePos = new ParticleSystem[Define.MAX_COL_NUM, Define.MAX_ROW_NUM];
 
     SpriteRenderer background;
     [HideInInspector] public Transform myTransform;
@@ -22,19 +22,19 @@ public class Pos : MonoBehaviour
         width = background.bounds.size.x / background.transform.localScale.x;
         height = background.bounds.size.y / background.transform.localScale.y; ;
 
-        float startX = (-1) * (width / 3);
-        float startY = (9 * height / (2 * 10));
+        float startX = (-1) * (width / Define.MAX_COL_NUM);
+        float startY = ((Define.MAX_ROW_NUM - 1) * height / ((Define.MAX_COL_NUM - 1) * Define.MAX_ROW_NUM));
         startPos = new Vector2(startX, startY);
 
         Managers.Game.selectedFrame.myTransform.position = startPos;
         Managers.Game.selectedFrame.myTransform.SetParent(myTransform);
 
-        for (int i = 0; i < 3; ++i)
+        for (int i = 0; i < Define.MAX_COL_NUM; ++i)
         {
-            float x = startPos.x + i * (width / 3);
-            for (int j = 0; j < 10; ++j)
+            float x = startPos.x + i * (width / Define.MAX_COL_NUM);
+            for (int j = 0; j < Define.MAX_ROW_NUM; ++j)
             {
-                float y = startPos.y - j * (height / 10);
+                float y = startPos.y - j * (height / Define.MAX_ROW_NUM);
 
                 GameObject go = Object.Instantiate(prefab);
                 blockPos[i, j] = go.transform;
